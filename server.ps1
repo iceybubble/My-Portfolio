6000425e73a4f9c7fb6e72c6c4cccda31ba205fa
iceybubble <kathaitpallavi164@gmail.com>
2026-07-28 07:51:22 +0530

    Add local PowerShell server script

---
+++ server.ps1
@@ -0,0 +1,49 @@
+$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add("http://localhost:8080/")
+$listener.Start()
+Write-Host "Serving d:\My-Portfolio on http://localhost:8080/"
+
+$mimeTypes = @{
+    ".html" = "text/html; charset=utf-8"
+    ".css"  = "text/css"
+    ".js"   = "text/javascript"
+    ".png"  = "image/png"
+    ".jpg"  = "image/jpeg"
+    ".jpeg" = "image/jpeg"
+    ".svg"  = "image/svg+xml"
+    ".json" = "application/json"
+    ".woff" = "font/woff"
+    ".woff2"= "font/woff2"
+}
+
+try {
+    while ($listener.IsListening) {
+        $context = $listener.GetContext()
+        $req = $context.Request
+        $res = $context.Response
+
+        $urlPath = $req.Url.LocalPath
+        if ($urlPath -eq "/") { $urlPath = "/index.html" }
+        $filePath = Join-Path "d:\My-Portfolio" ($urlPath.TrimStart('/').Replace('/', '\'))
+
+        if (Test-Path $filePath -PathType Leaf) {
+            $ext = [System.IO.Path]::GetExtension($filePath).ToLower()
+            if ($mimeTypes.ContainsKey($ext)) {
+                $res.ContentType = $mimeTypes[$ext]
+            } else {
+                $res.ContentType = "application/octet-stream"
+            }
+            $bytes = [System.IO.File]::ReadAllBytes($filePath)
+            $res.ContentLength64 = $bytes.Length
+            $res.OutputStream.Write($bytes, 0, $bytes.Length)
+        } else {
+            $res.StatusCode = 404
+            $buffer = [System.Text.Encoding]::UTF8.GetBytes("404 Not Found")
+            $res.ContentLength64 = $buffer.Length
+            $res.OutputStream.Write($buffer, 0, $buffer.Length)
+        }
+        $res.OutputStream.Close()
+    }
+} finally {
+    $listener.Stop()
+}
